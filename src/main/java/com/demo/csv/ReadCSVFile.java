06651b73576f4d67d6e53484a137228a2e7909f2
Mitu28 <mitukumari2024@gmail.com>
2026-01-13 15:14:11 -0600

    Practiced Mapping the CSV File to POJO

--- src/main/java/com/demo/csv/ReadCSVFile.java
+++ src/main/java/com/demo/csv/ReadCSVFile.java
@@ -9,24 +9,29 @@ import java.util.List;
 
 
 public class ReadCSVFile {
-    public static void main(String[]args) throws IOException, CsvException {
+    public static void main(String[] args) throws IOException, CsvException {
 
 
+        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("testData/LoginCreds.csv");
+        InputStreamReader isr = new InputStreamReader(is);
+        CSVReader csvReader = new CSVReader(isr);
 
-        InputStream is =    Thread.currentThread().getContextClassLoader().getResourceAsStream("testData/LoginCreds.csv");
-        InputStreamReader isr=new InputStreamReader(is);
-        CSVReader csvReader=new CSVReader(isr);
+        List<String[]> dataList = csvReader.readAll();
+        for (String[] dataArray : dataList) {
 
-        List <String[]> dataList=csvReader.readAll();
-        for(String[] dataArray:dataList){
-            System.out.println(dataArray[0]);
-            System.out.println(dataArray[1]);
+            // skip empty or invalid rows
+            if (dataArray == null || dataArray.length < 2) {
+                continue;
+            }
 
+            // skip header row
+            if ("username".equalsIgnoreCase(dataArray[0])) {
+                continue;
+            }
 
-
+            System.out.println(
+                    "Username: " + dataArray[0] + ", Password: " + dataArray[1]
+            );
         }
-
-
-
     }
 }
